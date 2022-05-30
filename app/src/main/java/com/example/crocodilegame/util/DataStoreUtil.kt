5957ff13c1f81944871b256a8ac1f534156c7627
okulovitchVladimir <uladzimir.akulovich@regula.by>
2022-05-30 22:29:56 +0300

    update all res, downgrade impl

--- app/src/main/java/com/example/crocodilegame/util/DataStoreUtil.kt
+++
@@ -1,6 +0,0 @@
-package com.example.crocodilegame.util
-
-import android.content.Context
-import androidx.datastore.preferences.preferencesDataStore
-
-val Context.dataStore by preferencesDataStore("settings")
