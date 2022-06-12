23cec55e975bae32575ed240eefe15dad0ad3ae9
okulovitchVladimir <uladzimir.akulovich@regula.by>
2022-06-12 22:05:57 +0300

    setup drawingView

--- app/src/main/java/com/okul/crocodile/ui/drawing/DrawingActivity.kt
+++ app/src/main/java/com/okul/crocodile/ui/drawing/DrawingActivity.kt
@@ -1,10 +1,14 @@
 package com.okul.crocodile.ui.drawing
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
+import com.okul.crocodile.R
 import com.okul.crocodile.databinding.ActivityDrawingBinding
+import com.okul.crocodile.util.Constants
+
 import dagger.hilt.android.AndroidEntryPoint
 import kotlinx.coroutines.flow.collect
 
@@ -26,10 +30,27 @@ class DrawingActivity : AppCompatActivity() {
         }
     }
 
+    private fun selectColor(color: Int) {
+        binding.drawingView.setColor(color)
+        binding.drawingView.setThickness(Constants.DEFAULT_PAINT_THICKNESS)
+    }
+
     private fun subscribeToUiStateUpdates() {
         lifecycleScope.launchWhenStarted {
             viewModel.selectedColorButtonId.collect { id ->
                 binding.colorGroup.check(id)
+                when (id) {
+                    R.id.rbBlack -> selectColor(Color.BLACK)
+                    R.id.rbBlue -> selectColor(Color.BLUE)
+                    R.id.rbGreen -> selectColor(Color.GREEN)
+                    R.id.rbOrange -> selectColor(getColor(R.color.orange))
+                    R.id.rbRed -> selectColor(Color.RED)
+                    R.id.rbYellow -> selectColor(Color.YELLOW)
+                    R.id.rbEraser -> {
+                        binding.drawingView.setColor(Color.WHITE)
+                        binding.drawingView.setThickness(60f)
+                    }
+                }
             }
         }
     }
